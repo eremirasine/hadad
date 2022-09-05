c94292d03f1c882e32cc8f726b397770763db59f
unknown <eremirasine@gmail.com>
2022-09-05 15:39:22 +0100

    change provier

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\URL;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -25,4 +26,13 @@ class AppServiceProvider extends ServiceProvider
     {
         //
     }
+   
+
+
+public function __construct(){
+    // Force SSL in production
+    if (config('app.env') == 'production') {
+        URL::forceScheme('https');
+    }
+}
 }
